7cc0664fb8b7c0e4cd95420e1b7b4e59fbec28be
iraas <iraas@monni.fyi>
2024-06-26 14:13:20 +0300

    cool

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from gevent.pywsgi import WSGIServer
 from main import app
 
-http_server = WSGIServer(("127.0.0.1", 8000), app)
-print("running at port 127.0.0.1:80")
+http_server = WSGIServer(("0.0.0.0", 8080), app)
+print("running at port 0.0.0.0:8080")
 http_server.serve_forever()
